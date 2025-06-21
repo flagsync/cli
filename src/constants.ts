25544a3c3f9fe986df960c77f1063762fe01a25b
Mike Chabot <mike.chabot@live.com>
2025-06-21 12:56:49 -0400

    Remove trailing slash

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,7 @@
 import os from 'os';
 import path from 'path';
 
-export const UI_BASE = 'https://www.flagsync.com/';
-export const API_BASE = 'https://api.flagsync.com/';
+export const UI_BASE = 'https://www.flagsync.com';
+export const API_BASE = 'https://api.flagsync.com';
 export const TOKEN_PATH = path.join(os.homedir(), '.flagsync-auth');
 export const JWT = '_hst';
