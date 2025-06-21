7fb4044d7cc18ae7115ca70e21decd08014a8740
Mike Chabot <mike.chabot@live.com>
2025-06-21 12:52:56 -0400

    Update constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,7 @@
 import os from 'os';
 import path from 'path';
 
-export const UI_BASE = 'http://localhost:3000'; // Next.js UI
-export const API_BASE = 'http://localhost:3001'; // NestJS backend
+export const UI_BASE = 'https://www.flagsync.com/';
+export const API_BASE = 'https://api.flagsync.com/';
 export const TOKEN_PATH = path.join(os.homedir(), '.flagsync-auth');
 export const JWT = '_hst';
